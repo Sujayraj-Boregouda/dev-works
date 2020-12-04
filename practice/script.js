3a0a039ca692a8d8a2d492c1ceafbd6d911f4372
Sujayraj-Boregouda <sujayraj.gouda@gmail.com>
2020-12-04 21:48:22 +0530

    List Practice

---
+++ practice/script.js
@@ -0,0 +1,22 @@
+
+
+var myMom = {
+   name: "gayatri",
+   age: 56,
+   mobile: 7760243439,
+}
+
+
+myMom.name="gayatri boregouda";
+
+myMom['location'] ="hubli";
+myMom['likes']="idli";
+
+delete myMom.likes;
+
+
+delete myMom.age;
+
+console.log(myMom)
+
+
